04bc658b8cb173f9fe0d82dd770685fd7b169d04
jadsonribeiro <jds_tj@hotmail.com>
2015-02-26 22:41:18 -0300

    setted password empty and fixed searching fields buttons

--- manipula_banco.php
+++ manipula_banco.php
@@ -5,21 +5,21 @@ include_once('class.Dados_banco.php');
 	
 $dados = new Dados_banco($_POST['nome'], $_POST['end_rua'], $_POST['end_bairro'], $_POST['end_numero'], $_POST['conta_agencia'], $_POST['conta_tipo'], $_POST['conta_numero'], $_POST['conta_saldo_inicial']);
 
-$conecta_banco = new ConectaBanco("localhost", "estudos_php", "root", "ab1936");
+$conecta_banco = new ConectaBanco("localhost", "estudos_php", "root", "");
 
-if(isset($_POST['btn_enviar'])) {
+if($_POST['btn'] == 'INSERIR') {
 	
 	$conecta_banco->inserir($dados);
 
-} elseif (isset($_POST['btn_atualizar'])) {
+} elseif ($_POST['btn'] = 'ATUALIZAR') {
 	
 	$conecta_banco->atualizar($dados);
 
-} elseif(isset($_POST['btn_deletar'])) {
+} elseif($_POST['btn'] == 'DELETAR') {
 	
 	$conecta_banco->deletar($_POST['nome']);
 	
 }
-
+	
 header("location:formulario.php");
 ?>
